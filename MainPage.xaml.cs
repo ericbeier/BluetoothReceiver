a489cfc26f87df6ccfd7232fb15b0c50ead4a6a2
joie95 <x>
2020-02-21 19:51:28 +0100

    test

---
+++ MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.Sockets;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+namespace blReceiver
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        RfcommServiceProvider provider;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Initialize the provider for the hosted RFCOMM service
+            provider = await RfcommServiceProvider.CreateAsync(RfcommServiceId.ObexObjectPush);
+
+            // Create a listener for this service and start listening
+            StreamSocketListener listener = new StreamSocketListener();
+            listener.ConnectionReceived += OnConnectionReceived;
+            await listener.BindServiceNameAsync(provider.ServiceId.AsString(), SocketProtectionLevel.BluetoothEncryptionAllowNullAuthentication);
+
+            // Set the SDP attributes and start advertising
+            InitializeServiceSdpAttributes(provider);
+            provider.StartAdvertising(listener);
+        }
+
+        const uint SERVICE_VERSION_ATTRIBUTE_ID = 0x0300;
+        const byte SERVICE_VERSION_ATTRIBUTE_TYPE = 0x0A;   // UINT32
+        const uint SERVICE_VERSION = 200;
+        void InitializeServiceSdpAttributes(RfcommServiceProvider provider)
+        {
+            var writer = new Windows.Storage.Streams.DataWriter();
+
+            // First write the attribute type
+            writer.WriteByte(SERVICE_VERSION_ATTRIBUTE_TYPE);
+            // Then write the data
+            writer.WriteUInt32(SERVICE_VERSION);
+
+            var data = writer.DetachBuffer();
+            provider.SdpRawAttributes.Add(SERVICE_VERSION_ATTRIBUTE_ID, data);
+        }
+
+        void OnConnectionReceived(StreamSocketListener listener, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            // Stop advertising/listening so that we're only serving one client
+            provider.StopAdvertising();
+            //await listener.Close();
+            var socket = args.Socket;
+
+
+            // The client socket is connected. At this point the App can wait for
+            // the user to take some action, for example, click a button to receive a file
+            // from the device, which could invoke the Picker and then save the
+            // received file to the picked location. The transfer itself would use
+            // the Sockets API and not the Rfcomm API, and so is omitted here for
+            // brevity.
+        }
+    }
+}
